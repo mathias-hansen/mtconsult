6a568889c9761fe0917090706603e315a0f00380
r0flbear <mathias.hansen783@gmail.com>
2014-11-25 11:41:00 +0100

    chaged slider from db to files

--- App_Code/slider.cs
+++ App_Code/slider.cs
@@ -2,35 +2,21 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
-
-public class image
-{
-    public string _filename { get; set; }
-    public string _description { get; set; }
-}
 
 public class slider
 {
-    public List<image> _images { get; set; }
+    public string[] _images { get; set; }
 
     public void getImages()
     {
-        DataAccess da = new DataAccess();
-        DataTable dt = new DataTable();
-        SqlCommand cmd = new SqlCommand();
-        _images = new List<image>();
-
-        cmd.CommandText = "SELECT * FROM mtconsult_slider";
-        dt = da.GetData(cmd);
-
-        foreach (DataRow row in dt.Rows)
+        _images = Directory.GetFiles(HttpContext.Current.Server.MapPath("/slider/images"));
+        for (int i = 0; i < _images.Length; i++)
         {
-            image img = new image();
-            img._filename = row["filename"].ToString();
-            img._description = row["description"].ToString();
-            _images.Add(img);
+            _images[i] = Regex.Match(_images[i], @"[\w\d\-_]+\.[\w\d\-_]+$").ToString();
         }
     }
 }
